f5502297721c62d995d78fa4b1813fec75516c07
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-25 18:44:09 -0700

    Update public-page.js

--- src/pages/public-page.js
+++ src/pages/public-page.js
@@ -7,6 +7,7 @@ export const PublicPage = () => {
   const [message, setMessage] = useState("");
 
   console.log(message);
+  console.log(setMessage);
 
   useEffect(() => {
     let isMounted = true;
