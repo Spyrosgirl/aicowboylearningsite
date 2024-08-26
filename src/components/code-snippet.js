491a51a78ace55fbab98984c4e846c47c416cee1
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-25 19:04:16 -0700

    Update code-snippet.js

--- src/components/code-snippet.js
+++ src/components/code-snippet.js
@@ -5,7 +5,7 @@ export const CodeSnippet = ({ title, code = "" }) => (
     <span className="code-snippet__title">{title}</span>
     <div className="code-snippet__container">
       <div className="code-snippet__wrapper">
-        <pre className="code-snippet__body">{code}</pre>
+        <button className="code-snippet__body">{code}</button>
       </div>
     </div>
   </div>
