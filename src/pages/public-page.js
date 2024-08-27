2e92e38774f307888f1bcb1df4f37744763b91b9
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-26 21:52:38 -0700

    Update public-page.js

--- src/pages/public-page.js
+++ src/pages/public-page.js
@@ -31,15 +31,15 @@ export const PublicPage = () => {
           Courses
         </h1>
         <div className="content__body">
-          {courses.map((course) => (
-            course.owned ? ( 
+          {courses.map((course) => 
+            course.owned ?( 
               <CourseCodeSnippet
                 key={course.courseID}
                 title={course.title}
                 desc={course.price}
               />
             ) : null
-          ))}
+          )}
         </div>
       </div>
     </PageLayout>
