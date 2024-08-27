1f02d1fb04be615cb9c28ef68a163adf0fb52067
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-26 20:42:20 -0700

    Update public-page.js

--- src/pages/public-page.js
+++ src/pages/public-page.js
@@ -2,9 +2,9 @@ import React from "react";
 import { PageLayout } from "../components/page-layout";
 
 const courses = [
-  {courseID: 0001, title: "deep learning for science and engineering", price: "$2.00", owned: False},
-  {courseID: 0002, title: "nvidia dli data science", price: "$5.00", owned: True},
-  {courseID: 0003, title: "nvidia dli deep", price: "$3.00", owned: False}
+  {courseID: 1, title: "deep learning for science and engineering", price: "$2.00", owned: false},
+  {courseID: 2, title: "nvidia dli data science", price: "$5.00", owned: true},
+  {courseID: 3, title: "nvidia dli deep", price: "$3.00", owned: false}
 ]
 
 export const PublicPage = () => {
