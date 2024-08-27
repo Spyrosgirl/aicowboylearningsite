7789c9b484f26c703e739331072d67c32882f247
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-26 22:07:36 -0700

    Update CourseCodeSnippet.js

--- src/components/CourseCodeSnippet.js
+++ src/components/CourseCodeSnippet.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-export const CourseCodeSnippet = ({ title, desc }) => (
+export const CourseCodeSnippet = ({ title, desc, price }) => (
   <div className="code-snippet">
     <span className="code-snippet__title">{title}</span>
+    <span className="code-snippet__title">{price}</span>
     <div className="code-snippet__container">
       <div className="code-snippet__wrapper">
         <button className="code-snippet__body">{desc}</button>
