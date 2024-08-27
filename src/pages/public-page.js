96505d9dabf4b8960167d8d4a75a09ec9d0069cb
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-26 20:18:27 -0700

    Update public-page.js

--- src/pages/public-page.js
+++ src/pages/public-page.js
@@ -8,9 +8,7 @@ export const PublicPage = () => {
         <h1 id="page-title" className="content__title">
           Courses
         </h1>
-        <div className="content__body">
-          This is a test
-        </div>
+        <div className="content__body">This is a test</div>
       </div>
     </PageLayout>
   );
