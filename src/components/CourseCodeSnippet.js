d41e142d641b9429076e77885d6d76484368fe11
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-26 22:28:09 -0700

    Update CourseCodeSnippet.js

--- src/components/CourseCodeSnippet.js
+++ src/components/CourseCodeSnippet.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const CourseCodeSnippet = ({ title, desc, price, link }) => {
   const handleClick = () => {
-    window.location.href = link;
+    window.open(link, '_blank');
   };
 
   return (
