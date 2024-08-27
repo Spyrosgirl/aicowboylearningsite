9cb871a44e5ec17e23a7c644a8415429885da996
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-26 20:17:22 -0700

    Update public-page.js

--- src/pages/public-page.js
+++ src/pages/public-page.js
@@ -1,36 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { CodeSnippet } from "../components/code-snippet";
+import React from "react";
 import { PageLayout } from "../components/page-layout";
-import { getPublicResource } from "../services/message.service";
 
 export const PublicPage = () => {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    let isMounted = true;
-
-    const getMessage = async () => {
-      const { data, error } = await getPublicResource();
-
-      if (!isMounted) {
-        return;
-      }
-
-      if (data) {
-        setMessage(JSON.stringify(data, null, 2));
-      }
-
-      if (error) {
-        setMessage(JSON.stringify(error, null, 2));
-      }
-    };
-
-    getMessage();
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);
-
   return (
     <PageLayout>
       <div className="content-layout">
@@ -38,7 +9,7 @@ export const PublicPage = () => {
           Courses
         </h1>
         <div className="content__body">
-          <CodeSnippet title="Courses" code={message} />
+          This is a test
         </div>
       </div>
     </PageLayout>
