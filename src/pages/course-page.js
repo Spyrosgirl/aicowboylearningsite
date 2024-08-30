79206c1b3e87d8ecc3e514a88737e0f7ea4fd465
Spyrosgirl <113068008+Spyrosgirl@users.noreply.github.com>
2024-08-30 16:31:58 -0700

    Create course-page.js

---
+++ src/pages/course-page.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { PageLayout } from "../components/page-layout";
+import { Dropdown } from "../components/dropdown";
+import { Prereq } from "../DLSE/prereq";
+
+export const CoursePage = () => {
+
+  return (
+    <PageLayout>
+    // Pull ID from Prev Page
+    <h1 className="CourseTitle">
+      Deep Learning for Science and Engineering Teaching Kit
+    </h1>
+    // Course PreReqs
+    <Dropdown title="Course Prerequisites>
+      <Prereq>
+      </Prereq>
+    </Dropdown>
+    // Drop Downs for files
+    </PageLayout>
+  );
+};
