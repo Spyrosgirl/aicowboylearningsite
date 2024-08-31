406f012696f3bb0b387035cda0ca4d91df5f55e1
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-30 18:04:52 -0700

    Update nav-bar-brand.js

--- src/components/navigation/desktop/nav-bar-brand.js
+++ src/components/navigation/desktop/nav-bar-brand.js
@@ -7,7 +7,7 @@ export const NavBarBrand = () => {
       <NavLink to="/">
         <img
           className="nav-bar__logo"
-          src="https://github.com/Spyrosgirl/aicowboylearningsite/blob/main/src/styles/components/Artboard%205-100.jpg?raw=true"
+          src="https://raw.githubusercontent.com/Spyrosgirl/aicowboylearningsite/main/src/styles/components/Artboard%205-100.jpg?token=GHSAT0AAAAAACWSG34FXBKV5Q2IIJRSNT2MZWSNROQ"
           alt="aicowboyslogo"
         />
       </NavLink>
