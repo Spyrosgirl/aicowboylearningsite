d58498ebd9990321f0aa02e7efe1cfa437c0091e
Spyrosgirl <113068008+Spyrosgirl@users.noreply.github.com>
2024-08-30 18:24:55 -0700

    Create module1.js

---
+++ src/components/courses/DLSE/module1.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Dropdown } from "../components/buttons/dropdown";
+
+export const Mod1 = () => {
+  return (
+    <>
+      <h2>Labs</h2>
+      <Dropdown title="1.2 Primer Python">
+        <p>Test</p>
+      </Dropdown>
+      <Dropdown title="1.3 Deep Learning Networks">
+        <p>Test</p>
+      </Dropdown>
+      <Dropdown title="1.4 Primer PFT Jax">
+        <p>Test</p>
+      </Dropdown>
+      <Dropdown title="1.5 Training and Optimization">
+        <p>Test</p>
+      </Dropdown>
+      <Dropdown title="1.6 Neural Network Architecture">
+        <p>Test</p>
+      </Dropdown>
+      <h2>Homeworks</h2>
+          <Dropdown title="1.2 Primer Python">
+        <p>Test</p>
+      </Dropdown>
+      <Dropdown title="1.3 Deep Learning Networks">
+        <p>Test</p>
+      </Dropdown>
+      <Dropdown title="1.4 Primer PFT Jax">
+        <p>Test</p>
+      </Dropdown>
+      <Dropdown title="1.5 Training and Optimization">
+        <p>Test</p>
+      </Dropdown>
+      <Dropdown title="1.6 Neural Network Architecture">
+        <p>Test</p>
+      </Dropdown>
+    </>
+  );
+};
