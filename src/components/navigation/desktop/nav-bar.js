5afb22ab5efe820bd6c311f7c0fea6ef51e43966
Spyrosgirl <113068008+Spyrosgirl@users.noreply.github.com>
2024-08-30 18:55:04 -0700

    Update nav-bar.js

--- src/components/navigation/desktop/nav-bar.js
+++ src/components/navigation/desktop/nav-bar.js
@@ -8,9 +8,7 @@ export const NavBar = () => {
     <div className="nav-bar__container">
       <nav className="nav-bar">
         <NavBarBrand />
-        <h1 className="nav-bar__brandtitle">
-          The Quantum Cowboys
-        </h1>
+        <h1 className="nav-bar__brandtitle">The Quantum Cowboys</h1>
         <NavBarTabs />
         <NavBarButtons />
       </nav>
