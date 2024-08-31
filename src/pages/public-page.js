51476328ee930b19078ace75a6293b2f5bdf66d4
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-30 17:16:41 -0700

    Update public-page.js

--- src/pages/public-page.js
+++ src/pages/public-page.js
@@ -52,20 +52,18 @@ export const PublicPage = () => {
         <h1 id="page-title" className="content__title">
           Your Courses
         </h1>
-        <div className="horizon">
-          <div className="content__body">
-            {courses.map((course) =>
-              course.owned ? (
-                <CourseCodeSnippet
-                  key={course.courseID}
-                  title={course.title}
-                  desc={course.desc}
-                  price={course.price}
-                  link={course.link}
-                />
-              ) : null
-            )}
-          </div>
+        <div className="content__body">
+          {courses.map((course) =>
+            course.owned ? (
+              <CourseCodeSnippet
+                key={course.courseID}
+                title={course.title}
+                desc={course.desc}
+                price={course.price}
+                link={course.link}
+              />
+            ) : null
+          )}
         </div>
         <h2>More courses we think you would like</h2>
         <div className="horizon">
