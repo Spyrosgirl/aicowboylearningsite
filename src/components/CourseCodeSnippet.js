3f3d4006f503d95ea4e73dde4140be1b9d99eb4f
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-31 17:49:27 -0700

    Update CourseCodeSnippet.js

--- src/components/CourseCodeSnippet.js
+++ src/components/CourseCodeSnippet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const CourseCodeSnippet = ({ title, desc, price, link }) => {
   const handleClick = () => {
@@ -6,14 +7,22 @@ export const CourseCodeSnippet = ({ title, desc, price, link }) => {
   };
 
   return (
-    <div onClick={handleClick} className="code-snippet">
-      <div className="code-snippet__title">
-        <span>{title}</span>
-        <span>{price}</span>
+   <NavLink
+        to={path}
+        end
+        className={({ isActive }) =>
+          "nav-bar__tab " + (isActive ? "nav-bar__tab--active" : "")
+        }
+      >
+      <div className="code-snippet">
+        <div className="code-snippet__title">
+          <span>{title}</span>
+          <span>{price}</span>
+        </div>
+        <div className="code-snippet__container">
+          <div className="code-snippet__body">{desc}</div>
+        </div>
       </div>
-      <div className="code-snippet__container">
-        <div className="code-snippet__body">{desc}</div>
-      </div>
-    </div>
+    </NavLink>
   );
 };
