e3900a9f2e8812ad0992bc74a4a64edd134afb14
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-08-31 17:52:35 -0700

    Update nav-bar-tab.js

--- src/components/navigation/desktop/nav-bar-tab.js
+++ src/components/navigation/desktop/nav-bar-tab.js
@@ -4,11 +4,11 @@ import { NavLink } from "react-router-dom";
 export const NavBarTab = ({ path, label }) => {
   return (
     <NavLink
-      to={path}
-      end
-      className={({ isActive }) =>
-        "nav-bar__tab " + (isActive ? "nav-bar__tab--active" : "")
-      }
+    to="/course"
+    end
+    className={({ isActive }) =>
+      "nav-bar__tab " + (isActive ? "nav-bar__tab--active" : "")
+    }
     >
       {label}
     </NavLink>
