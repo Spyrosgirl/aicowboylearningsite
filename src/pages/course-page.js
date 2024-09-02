e5287f268055f08e400af3efbe7a1441eaad7aae
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-02 15:45:58 -0700

    Update course-page.js

--- src/pages/course-page.js
+++ src/pages/course-page.js
@@ -8,8 +8,8 @@ import { Prereq } from "../components/courses/DLSE/prereq";
 import { Mod1 } from "../components/courses/DLSE/module1";
 
 export const CoursePage = () => {
-const location = useLocation();
-const { course } = location.state || {};
+  const location = useLocation();
+  const { course } = location.state || {};
   return (
     <PageLayout>
       {/* Pull ID from Prev Page */}
