fe12759f22aeb9cd6d11a43ef5b915288ff7cedf
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-02 15:26:30 -0700

    Update course-page.js

--- src/pages/course-page.js
+++ src/pages/course-page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { PageLayout } from "../components/page-layout";
 import { Dropdown } from "../components/buttons/dropdown";
 /* pull specific import*/
@@ -6,12 +7,15 @@ import { Prereq } from "../components/courses/DLSE/prereq";
 import { Mod1 } from "../components/courses/DLSE/module1";
 
 export const CoursePage = () => {
+  const location = useLocation();
+  const { course } = location.state || {}
   return (
     <PageLayout>
       {/* Pull ID from Prev Page */}
       <h1 className="CourseTitle">
         Deep Learning for Science and Engineering Teaching Kit
       </h1>
+      <p> ID: {course} </p>
       {/* Course PreReqs */}
       <Dropdown title="Course Prerequisites">
         <Prereq />
