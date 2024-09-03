aa3de8ffbefc07eb497e87ac886a1cb33e493fd1
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-03 16:08:08 -0700

    Update courses_list.js

--- src/components/courses_list.js
+++ src/components/courses_list.js
@@ -1,7 +1,7 @@
 export const Courses = [
   {
     courseID: 1,
-    title: "Deep Learning for Science and Engineering Teaching Kit",
+    title: "Deep Learning for Science and Engineering Kit",
     link: "../components/courses/DLSE/prereq",
     desc: "This is a long description to test how the CSS reacts. This is temporary and should be removed at a later date. Do not leave this there. This is not a real description.",
     price: "$2.00",
@@ -9,7 +9,7 @@ export const Courses = [
   },
   {
     courseID: 2,
-    title: "NVIDIA DLI Data Science",
+    title: "NVIDIA DLI Data Science Kit",
     link: "https://drive.google.com/drive/folders/1bIQISTier9NlpXNSHlgm10TxCs8msA9n?usp=drive_link",
     desc: "Course Two Desc",
     price: "$5.00",
@@ -17,7 +17,7 @@ export const Courses = [
   },
   {
     courseID: 3,
-    title: "NVIDIA DLI Deep",
+    title: "NVIDIA DLI Deep Learning Kit",
     link: "https://drive.google.com/drive/folders/1dgGK5ABHKBH9foCXDjnHW5dnICmCmYsM?usp=drive_link",
     desc: "Course Three Desc",
     price: "$3.00",
@@ -25,7 +25,7 @@ export const Courses = [
   },
   {
     courseID: 4,
-    title: "NVIDIA DLI Deep 2",
+    title: "NVIDIA DLI Deep Edge AI and Robotics Kit",
     link: "https://drive.google.com/drive/folders/1dgGK5ABHKBH9foCXDjnHW5dnICmCmYsM?usp=drive_link",
     desc: "This is another long description just in case. I like testing things. I like to be thorough.",
     price: "$4.00",
@@ -33,7 +33,7 @@ export const Courses = [
   },
   {
     courseID: 5,
-    title: "NVIDIA Dli Deep 3",
+    title: "NVIDIA Dli GPU Teaching Kit",
     link: "https://drive.google.com/drive/folders/1dgGK5ABHKBH9foCXDjnHW5dnICmCmYsM?usp=drive_link",
     desc: "This is another description",
     price: "$4.00",
