e828d8e9f6c74ff19c3592b8cc200a580b1a59df
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-02 20:10:05 -0700

    Update module1.js

--- src/components/courses/DLSE/module1.js
+++ src/components/courses/DLSE/module1.js
@@ -24,21 +24,22 @@ export const Mod1 = ({ id }) => {
           </Dropdown>
         ))}
       <h2>Homeworks</h2>
-      <Dropdown title="1.2 Primer Python">
-        <p>Test</p>
-      </Dropdown>
-      <Dropdown title="1.3 Deep Learning Networks">
-        <p>Test</p>
-      </Dropdown>
-      <Dropdown title="1.4 Primer PFT Jax">
-        <p>Test</p>
-      </Dropdown>
-      <Dropdown title="1.5 Training and Optimization">
-        <p>Test</p>
-      </Dropdown>
-      <Dropdown title="1.6 Neural Network Architecture">
-        <p>Test</p>
-      </Dropdown>
+      {savedCourse &&
+        savedCourse.title.map((_, idx) => (
+          <Dropdown key={idx} title={savedCourse.title[idx]}>
+            {savedCourse.labLink[idx].map((_, idx2) => (
+              <a
+                href={savedCourse.workLink[idx][idx2]}
+                target="_blank"
+                rel="noreferrer"
+                className="lab_links"
+                key={idx2}
+              >
+                {savedCourse.workDesc[idx][idx2]}
+              </a>
+            ))}
+          </Dropdown>
+        ))}
     </>
   );
 };
