45f2eba8eb349df6c5f30a49797b69feb3972db2
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-02 17:07:45 -0700

    Update module1.js

--- src/components/courses/DLSE/module1.js
+++ src/components/courses/DLSE/module1.js
@@ -16,7 +16,7 @@ export const Mod1 = () => {
   return (
     <>
       <h2>Labs</h2>
-      <Dropdown title={courses[0]}>
+      <Dropdown title="temp title">
         <a
           href="https://colab.research.google.com/drive/158mKtWkbrdZwxNoot1cta2d0Kr-eOjbt"
           target="_blank"
