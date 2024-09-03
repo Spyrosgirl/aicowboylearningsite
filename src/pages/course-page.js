2cf464cabe69b58302ec634944d6a0e15803bba4
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-02 19:19:40 -0700

    Update course-page.js

--- src/pages/course-page.js
+++ src/pages/course-page.js
@@ -20,7 +20,7 @@ export const CoursePage = () => {
         <Prereq />
       </Dropdown>
       <Dropdown title="Module 1 - Basics">
-        <Mod1 />
+        <Mod1 id={course}/>
       </Dropdown>
       {/* Drop Downs for files */}
     </PageLayout>
