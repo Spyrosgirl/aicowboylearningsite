e2d78835c7f831607a3265cd690626f365844cd2
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-03 19:21:19 -0700

    Update module1.js

--- src/components/courses/DLSE/module1.js
+++ src/components/courses/DLSE/module1.js
@@ -31,20 +31,17 @@ export const Mod1 = ({ id }) => {
           <h2>Homeworks</h2>
           {savedCourse.title.map((_, idx) => (
             <Dropdown key={idx} title={savedCourse.title[idx]}>
-              {savedCourse.workLink[idx].map((_, idx2) => (
-                <>
-                  {savedCourse.workLink[idx][idx2] != "" && (
-                    <a
-                      href={savedCourse.workLink[idx][idx2]}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="lab_links"
-                      key={idx2}
-                    >
-                      {savedCourse.workDesc[idx][idx2]}
-                    </a>
-                  )}
-                </>
+              {savedCourse.workLink[idx][0] != "" &&
+                savedCourse.workLink[idx].map((_, idx2) => (
+                <a
+                  href={savedCourse.workLink[idx][idx2]}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="lab_links"
+                  key={idx2}
+                >
+                  {savedCourse.workDesc[idx][idx2]}
+                </a>
               ))}
             </Dropdown>
           ))}
