2c644cd1becca5fe50f89815cafa0933208880d8
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-03 18:58:53 -0700

    Update module1.js

--- src/components/courses/DLSE/module1.js
+++ src/components/courses/DLSE/module1.js
@@ -23,24 +23,27 @@ export const Mod1 = ({ id }) => {
             ))}
           </Dropdown>
         ))}
-      <h2>Homeworks</h2>
-      {savedCourse &&
-        savedCourse.workLink[0] != "" &&
-        savedCourse.title.map((_, idx) => (
-          <Dropdown key={idx} title={savedCourse.title[idx]}>
-            {savedCourse.labLink[idx].map((_, idx2) => (
-              <a
-                href={savedCourse.workLink[idx][idx2]}
-                target="_blank"
-                rel="noreferrer"
-                className="lab_links"
-                key={idx2}
-              >
-                {savedCourse.workDesc[idx][idx2]}
-              </a>
-            ))}
-          </Dropdown>
-        ))}
+
+      {savedCourse && savedCourse.workLink[0] !== "" && (
+        <>
+          <h2>Homeworks</h2>
+          {savedCourse.title.map((_, idx) => (
+              <Dropdown key={idx} title={savedCourse.title[idx]}>
+                {savedCourse.labLink[idx].map((_, idx2) => (
+                  <a
+                    href={savedCourse.workLink[idx][idx2]}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="lab_links"
+                    key={idx2}
+                  >
+                    {savedCourse.workDesc[idx][idx2]}
+                  </a>
+                ))}
+              </Dropdown>
+            )}
+        </>
+      )}
     </>
   );
 };
