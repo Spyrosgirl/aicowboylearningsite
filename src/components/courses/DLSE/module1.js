c3e7e9662fce7e0b6fb0b260b1c055127ab0f926
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-03 19:08:55 -0700

    Update module1.js

--- src/components/courses/DLSE/module1.js
+++ src/components/courses/DLSE/module1.js
@@ -6,26 +6,26 @@ export const Mod1 = ({ id }) => {
   const savedCourse = courses.find((course) => course.courseID === id);
   return (
     <>
-    {savedCourse && savedCourse.labLink[0] != "" && (
-      <>
-        <h2>Labs</h2>
-        {savedCourse.title.map((_, idx) => (
-          <Dropdown key={idx} title={savedCourse.title[idx]}>
-            {savedCourse.labLink[idx].map((_, idx2) => (
-              <a
-                href={savedCourse.labLink[idx][idx2]}
-                target="_blank"
-                rel="noreferrer"
-                className="lab_links"
-                key={idx2}
-              >
-                {savedCourse.labDesc[idx][idx2]}
-              </a>
-            ))}
-          </Dropdown>
-        ))}
-      </>
-    )}
+      {savedCourse && savedCourse.labLink[0] != "" && (
+        <>
+          <h2>Labs</h2>
+          {savedCourse.title.map((_, idx) => (
+            <Dropdown key={idx} title={savedCourse.title[idx]}>
+              {savedCourse.labLink[idx].map((_, idx2) => (
+                <a
+                  href={savedCourse.labLink[idx][idx2]}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="lab_links"
+                  key={idx2}
+                >
+                  {savedCourse.labDesc[idx][idx2]}
+                </a>
+              ))}
+            </Dropdown>
+          ))}
+        </>
+      )}
       {savedCourse && savedCourse.workLink[0] != "" && (
         <>
           <h2>Homeworks</h2>
