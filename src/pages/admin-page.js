db0c6ca9fdb4b5f3deb2aa4e31e0534fc7d05ef1
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-06 13:51:40 -0700

    Update admin-page.js

--- src/pages/admin-page.js
+++ src/pages/admin-page.js
@@ -1,31 +1,30 @@
 import React from "react";
 import { CodeSnippet } from "../components/code-snippet";
 import { PageLayout } from "../components/page-layout";
+import { CourseCodeSnippet } from "../components/CourseCodeSnippet";
+import { Courses } from "../components/courses_list";
 
 export const AdminPage = () => {
   return (
     <PageLayout>
       <div className="content-layout">
         <h1 id="page-title" className="content__title">
-          Admin Page
+          Your Courses
         </h1>
-        <div className="content__body">
-          <p id="page-description">
-            <span>
-              This page retrieves an <strong>admin message</strong> from an
-              external API.
-            </span>
-            <span>
-              <strong>
-                Only authenticated users with the{" "}
-                <code>read:admin-messages</code> permission should access this
-                page.
-              </strong>
-            </span>
-          </p>
-          <CodeSnippet title="Admin Message" code="Message" />
+        <div className="horizon">
+          {Courses.map((course) =>
+            course.owned ? (
+              <CourseCodeSnippet
+                key={course.courseID}
+                title={course.title}
+                desc={course.desc}
+                price={course.price}
+                link={course.link}
+                ID={course.courseID}
+              />
+            ) : null
+          )}
         </div>
-      </div>
     </PageLayout>
   );
 };
