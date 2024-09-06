715d80808ec8980430d25cc53b7d579383d9be22
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-06 14:24:46 -0700

    Create edit-course-page.js

---
+++ src/pages/edit-course-page.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { PageLayout } from "../components/page-layout";
+import { Dropdown } from "../components/buttons/dropdown";
+import { Prereq } from "../components/courses/DLSE/prereq";
+import { Mod1 } from "../components/courses/DLSE/module1";
+import { Courses } from "../components/courses_list";
+
+export const CoursePage = () => {
+  const location = useLocation();
+  const { courseID } = location.state || {};
+  return (
+    <PageLayout>
+      {/* Pull ID from Prev Page */}
+      <h1 className="CourseTitle">
+        {Courses.find((course) => course.courseID === courseID).title}
+      </h1>
+      {/* Course PreReqs */}
+      <Dropdown title="Course Prerequisites">
+        <Prereq />
+      </Dropdown>
+      <Dropdown title="Module 1 - Basics">
+        <Mod1 id={courseID} />
+      </Dropdown>
+      {/* Drop Downs for files */}
+    </PageLayout>
+  );
+};
