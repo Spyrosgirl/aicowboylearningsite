f4239ee212b98d35d9c2719c51b35ecabebc248b
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-06 18:38:30 -0700

    Update add-lab-page.js

--- src/pages/add-lab-page.js
+++ src/pages/add-lab-page.js
@@ -3,13 +3,22 @@ import { PageLayout } from "../components/page-layout";
 import { LabElement } from "../components/lab-element";
 //import { Courses } from "../components/courses_list";
 export const AddLabPage = () => {
+  const [labElements, setLabElements] = useState([<LabElement key={0} />]);
+
+  const addLabElem = () => {
+    setLabElem((prevElem) => [
+      ...prevElem,
+      <LabElement key={prevElements.length} />
+    ]);
+  };
+  
   return (
     <PageLayout>
       <div>
         <label htmlFor="labName">Lab Group Name:</label>
         <input type="text" id="labName" name="labName" />
       </div>
-      <LabElement> </LabElement>
+      {labElements}
       <button> Add Another Lab </button>
     </PageLayout>
   );
