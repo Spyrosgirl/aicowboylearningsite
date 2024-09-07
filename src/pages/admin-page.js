2ee2419936c5330bcaa3789e87987564a56f863a
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-07 16:23:44 -0700

    Update admin-page.js

--- src/pages/admin-page.js
+++ src/pages/admin-page.js
@@ -10,10 +10,7 @@ export const AdminPage = () => {
   return (
     <PageLayout>
       <div className="admin_buttons_container">
-        <div
-          className="admin_button"
-          onClick={() => navTo("/adminCourses")}
-        >
+        <div className="admin_button" onClick={() => navTo("/adminCourses")}>
           Manage Course
         </div>
       </div>
