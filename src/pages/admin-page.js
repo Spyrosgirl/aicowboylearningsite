2377dfc435221833c88e6fca77ff078bda3d9302
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-07 15:12:24 -0700

    Update admin-page.js

--- src/pages/admin-page.js
+++ src/pages/admin-page.js
@@ -1,28 +1,10 @@
 import React from "react";
 import { PageLayout } from "../components/page-layout";
-import { CourseCodeSnippet } from "../components/CourseCodeSnippet";
-import { Courses } from "../components/courses_list";
 
 export const AdminPage = () => {
   return (
     <PageLayout>
-      <div className="content-layout">
-        <h1 id="page-title" className="content__title">
-          Your Courses
-        </h1>
-        <div className="horizon">
-          {Courses.map((course) => (
-            <CourseCodeSnippet
-              key={course.courseID}
-              title={course.title}
-              desc={course.desc}
-              price={course.price}
-              link="/editCourse"
-              ID={course.courseID}
-            />
-          ))}
-        </div>
-      </div>
+      <div onClick="{ navigate('/adminCourses'); }"> Manage Course </div>
     </PageLayout>
   );
 };
