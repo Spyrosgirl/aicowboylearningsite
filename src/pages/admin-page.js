108d44048440abcaa929c6fbb0e6cd967a31579b
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-07 15:14:04 -0700

    Update admin-page.js

--- src/pages/admin-page.js
+++ src/pages/admin-page.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { PageLayout } from "../components/page-layout";
+import { useNavigate } from "react-router-dom";
 
 export const AdminPage = () => {
+  const navigate = useNavigate();
   return (
     <PageLayout>
       <div onClick="{ navigate('/adminCourses'); }"> Manage Course </div>
