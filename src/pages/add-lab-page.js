b2eae21df382ca75dfdac4bf7701ca8f46a93955
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-06 17:29:42 -0700

    Update add-lab-page.js

--- src/pages/add-lab-page.js
+++ src/pages/add-lab-page.js
@@ -3,6 +3,9 @@ import { PageLayout } from "../components/page-layout";
 //import { Courses } from "../components/courses_list";
 export const AddLabPage = () => {
   return (
-    <PageLayout></PageLayout>
+    <PageLayout>
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname"><br><br>
+    </PageLayout>
   );
 };
