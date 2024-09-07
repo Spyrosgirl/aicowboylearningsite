e6697125da444151de7a57a129b75a037cb52fbd
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-06 18:57:19 -0700

    Update lab-element.js

--- src/components/lab-element.js
+++ src/components/lab-element.js
@@ -3,11 +3,11 @@ import React from "react";
 export const LabElement = () => {
   return (
     <div>
-      <div>
+      <div className="lab-input-container">
         <label htmlFor="labTitle">Lab Title:</label>
         <input type="text" id="labTitle" name="labTitle" />
       </div>
-      <div>
+      <div className="lab-input-container">
         <label htmlFor="labLink">Lab Link:</label>
         <input type="labLink" id="labLink" name="labLink" />
       </div>
