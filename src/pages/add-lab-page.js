40eddf85a77f3d4bd8a46a959bdd76d54e2db125
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-06 17:23:36 -0700

    Update add-lab-page.js

--- src/pages/add-lab-page.js
+++ src/pages/add-lab-page.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { PageLayout } from "../components/page-layout";
 //import { Courses } from "../components/courses_list";
-
 export const AddLabPage = () => {
   return (
-    <PageLayout>
-      
-    </PageLayout>
+    <PageLayout></PageLayout>
   );
 };
