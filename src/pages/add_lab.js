088b39661ae4573310534066d5444d3bea35a22b
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-06 17:11:04 -0700

    Create add_lab.js

---
+++ src/pages/add_lab.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { PageLayout } from "../components/page-layout";
+import { CourseCodeSnippet } from "../components/CourseCodeSnippet";
+import { Courses } from "../components/courses_list";
+
+export const AdminPage = () => {
+  return (
+    <PageLayout>
+      <div className="content-layout">
+        <h1 id="page-title" className="content__title">
+          Your Courses
+        </h1>
+        <div className="horizon">
+          {Courses.map((course) => (
+            <CourseCodeSnippet
+              key={course.courseID}
+              title={course.title}
+              desc={course.desc}
+              price={course.price}
+              link="/editCourse"
+              ID={course.courseID}
+            />
+          ))}
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
