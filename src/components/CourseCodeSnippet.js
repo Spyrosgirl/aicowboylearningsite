212379e223d817d64407f1b174fd60aa32ae1c22
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-07 15:09:57 -0700

    Update CourseCodeSnippet.js

--- src/components/CourseCodeSnippet.js
+++ src/components/CourseCodeSnippet.js
@@ -4,13 +4,7 @@ import { NavLink } from "react-router-dom";
 export const CourseCodeSnippet = ({ title, desc, price, link, ID }) => {
   return (
     <NavLink to={link} end state={{ courseID: ID }} className="code-snippet">
-      <div className="code-snippet__title">
-        <span>{title}</span>
-        <span>{price}</span>
-      </div>
-      <div className="code-snippet__container">
-        <div className="code-snippet__body">{desc}</div>
-      </div>
+      <div onClick="{ navigate('/adminCourses'); }"> Manage Course </div>
     </NavLink>
   );
 };
