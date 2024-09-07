29bfbcde8df45394b1ce1dee3344b4b587715065
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-07 15:23:27 -0700

    Update admin-page.js

--- src/pages/admin-page.js
+++ src/pages/admin-page.js
@@ -9,7 +9,7 @@ export const AdminPage = () => {
   };
   return (
     <PageLayout>
-      <div onClick={navTo('/adminCourses')}> Manage Course </div>
+      <div onClick={navTo("/adminCourses")}> Manage Course </div>
     </PageLayout>
   );
 };
