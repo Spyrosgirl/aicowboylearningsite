61fb2a84751c1fb5be287b67a4a0cf95b5c3ab05
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-06 17:22:46 -0700

    Update add-lab-page.js

--- src/pages/add-lab-page.js
+++ src/pages/add-lab-page.js
@@ -1,28 +1,11 @@
 import React from "react";
 import { PageLayout } from "../components/page-layout";
-import { CourseCodeSnippet } from "../components/CourseCodeSnippet";
-import { Courses } from "../components/courses_list";
+//import { Courses } from "../components/courses_list";
 
 export const AddLabPage = () => {
   return (
     <PageLayout>
-      <div className="content-layout">
-        <h1 id="page-title" className="content__title">
-          Your Courses
-        </h1>
-        <div className="horizon">
-          {Courses.map((course) => (
-            <CourseCodeSnippet
-              key={course.courseID}
-              title={course.title}
-              desc={course.desc}
-              price={course.price}
-              link="/editCourse"
-              ID={course.courseID}
-            />
-          ))}
-        </div>
-      </div>
+      
     </PageLayout>
   );
 };
