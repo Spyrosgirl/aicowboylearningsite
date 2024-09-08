f98feebaf5e09d399bf46b1b63583ad88d290b51
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-07 18:15:21 -0700

    Update module1.js

--- src/components/courses/DLSE/module1.js
+++ src/components/courses/DLSE/module1.js
@@ -30,23 +30,25 @@ export const Mod1 = ({ id, edit }) => {
           {savedCourse.title.map((_, idx) => (
             <Dropdown key={idx} title={savedCourse.title[idx]}>
               {savedCourse.labLink[idx].map((_, idx2) => (
-                {edit && (
-                  <div
-                    className="inner-module-button blueButton"
-                    onClick={handleNewLab}
+                <React.Fragment key={idx2}>
+                  {edit && (
+                    <div
+                      className="inner-module-button blueButton"
+                      onClick={handleNewLab}
+                    >
+                      Add New Link
+                    </div>
+                  )}
+                  <a
+                    href={savedCourse.labLink[idx][idx2]}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="lab_links"
+                    key={idx2}
                   >
-                    Add New Link
-                  </div>
-                )}
-                <a
-                  href={savedCourse.labLink[idx][idx2]}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="lab_links"
-                  key={idx2}
-                >
-                  {savedCourse.labDesc[idx][idx2]}
-                </a>
+                    {savedCourse.labDesc[idx][idx2]}
+                  </a>
+                </React.Fragment>
               ))}
             </Dropdown>
           ))}
