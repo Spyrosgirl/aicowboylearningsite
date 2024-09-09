775113c80706a6c506964e2194a6a6152584aa50
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-09 16:13:35 -0700

    Update nav-bar.js

--- src/components/navigation/desktop/nav-bar.js
+++ src/components/navigation/desktop/nav-bar.js
@@ -5,20 +5,13 @@ import { NavBarTabs } from "./nav-bar-tabs";
 
 export const NavBar = () => {
   return (
-    <>
-      <link
-        rel="icon"
-        href="https://raw.githubusercontent.com/Spyrosgirl/aicowboylearningsite/main/src/components/navigation/desktop/IMG_4078.png"
-        type="image/x-icon"
-      />
-      <div className="nav-bar__container">
-        <nav className="nav-bar">
-          <NavBarBrand />
-          <h1 className="nav-bar__brandtitle">The Quantum Cowboys</h1>
-          <NavBarTabs />
-          <NavBarButtons />
-        </nav>
-      </div>
-    </>
+    <div className="nav-bar__container">
+      <nav className="nav-bar">
+        <NavBarBrand />
+        <h1 className="nav-bar__brandtitle">The Quantum Cowboys</h1>
+        <NavBarTabs />
+        <NavBarButtons />
+      </nav>
+    </div>
   );
 };
