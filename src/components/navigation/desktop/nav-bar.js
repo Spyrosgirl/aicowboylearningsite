b782de988c297a347c6a2afc9fbc96d856880b02
19Nightowl <72186944+19Nightowl@users.noreply.github.com>
2024-09-09 16:07:27 -0700

    Update nav-bar.js

--- src/components/navigation/desktop/nav-bar.js
+++ src/components/navigation/desktop/nav-bar.js
@@ -5,6 +5,11 @@ import { NavBarTabs } from "./nav-bar-tabs";
 
 export const NavBar = () => {
   return (
+     <link
+      rel="icon"
+      href="https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200X200.png"
+      type="image/x-icon"
+      />
     <div className="nav-bar__container">
       <nav className="nav-bar">
         <NavBarBrand />
